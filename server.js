5ab3fdbe9c87e15e264acc3f63b14cc9250901b0
g g <gabedotgutierrez@gmail.com>
2024-09-20 17:15:34 -0400

    3rd task done, big case made

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const collectibles = [
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
 
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
 app.listen(3000, () => {
     console.log('In the year 3000')
@@ -21,11 +30,46 @@ app.get("/greetings/:username", (req,res)=>{
 // Task: Set up a route to handle URLs following the pattern /roll/<number-parameter>.
 
 app.get("/roll/:number",(req,res)=>{
-    res.end(`you rolled a ${Math.floor(Math.random()*req.params.number)}`)
+    res.send(`you rolled a ${Math.floor(Math.random()*req.params.number)}`)
 })
 
 // Task: Create a route for URLs like /collectibles/<index-parameter>.
 
 app.get("/collectibles/:index",(req,res)=>{
-    res.end(`You can get a ${collectibles[req.params.index].name} for the low low price of ${collectibles[req.params.index].price}$ CALL NOW!!!`)
+    if (req.params.index > 0 && req.params.index < collectibles.length){
+    res.send(`You can get a ${collectibles[req.params.index].name} for the low low price of ${collectibles[req.params.index].price}$ CALL NOW!!!`)
+    } else {
+    res.send("Somethings gone terribly wrong :(")
+    }
+})
+
+// app.get("/collectibles/",(req,res)=>{
+//     res.send(`This item is not yet in stock. Check back soon!`)
+// })
+
+// Task: Create a route /shoes that filters the list of shoes based on query parameters.
+
+/*
+Query Parameters:
+
+min-price: Excludes shoes below this price.
+max-price: Excludes shoes above this price.
+type: Shows only shoes of the specified type.
+No parameters: Responds with the full list of shoes.
+*/
+
+// app.get("/shoes/", (req,res) => {
+//     const min = req.query.min-price
+//     const max = req.query.max-price
+//     const type = req.query.type
+// })
+
+app.get("/shoes", (req,res) => {
+    let results = ""
+
+    shoes.forEach((shoe, i) => {
+        results += `${i + 1}. ${shoe.name} `
+    })
+    res.send(results)
+    // res.end(`${shoes[1].name}`)
 })
