ad92f4a916773b6f6df13e90a46a4bcd6c7956fc
g g <gabedotgutierrez@gmail.com>
2024-09-20 16:48:37 -0400

    3rd task done

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express")
 const app = express()
 
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+
 app.listen(3000, () => {
     console.log('In the year 3000')
   })
@@ -15,4 +22,10 @@ app.get("/greetings/:username", (req,res)=>{
 
 app.get("/roll/:number",(req,res)=>{
     res.end(`you rolled a ${Math.floor(Math.random()*req.params.number)}`)
+})
+
+// Task: Create a route for URLs like /collectibles/<index-parameter>.
+
+app.get("/collectibles/:index",(req,res)=>{
+    res.end(`You can get a ${collectibles[req.params.index].name} for the low low price of ${collectibles[req.params.index].price}$ CALL NOW!!!`)
 })
